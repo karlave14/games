e29a36d91ac6eee52b032f61c4710436032f9964
karlave14 <karla_valdeze14@hotmail.com>
2020-03-31 01:01:48 -0600

    listo

--- hangman.c
+++ hangman.c
@@ -1,98 +1,150 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <time.h>
 
-	char hits[100];
-        char fails[100];
-        int attempt = 6;
-        int num_hits=0;
-        int num fails=0;
-        int size = 0;
 // mostrar instrucciones
-void instruction(){
+void instruction()
+{
+
 	printf("WELCOME TO HAMGMAN\n");
 	printf("*********************\n");
 	printf("You have 5 lives\n");
 	printf("There are some word that could come out: \n");
-        printf("words:\n");
-        printf("    Future\n");
-        printf("    Rainbow\n");
-        printf("    Wedding\n");
-        printf("    Strawberry\n");
-        printf("    Nostalgia\n");
-
+	printf("words:\n");
+	printf("    Future\n");
+	printf("    Rainbow\n");
+	printf("    Wedding\n");
+	printf("    Strawberry\n");
+	printf("    Nostalgia\n");
 }
+
 // palabra al azar
-int ramdom(int stop1, int stop2){
-	int valor=rand()% stop1 + stop2;
+int ramdom(int stop1)
+{
+	int valor = rand() % stop1;
 	return valor;
 }
-	char *get_ramdon(char *arreglo[], int size){
-	int index = ramdon(size-1,0);
-	char *word=arreglo[index];
-return word;
+
+char *get_ramdon(char *arreglo[], int size)
+{
+
+	int index = ramdom(size);
+	char *word = arreglo[index];
+	return word;
 }
-void hiting_hits(char letter){
-	hits[num_hits]=letter;
-	num_hits++;
-	if(num_hits==size){
-	printf("Congratulations the word is: %s\n", &word);
-	exit(0);
+
+void hiting_hits(char *word, int *num_hits)
+{
+
+	// Aunmenta en uno la cantidad de aciertos a través del puntero.
+	*num_hits = *num_hits + 1;
+
+	//  Si la cantidad de aciertos es igual a la
+	// 	cantidad de letras de la palabra:
+	// 	mostrar al jugador que ya ganó.
+	
+	if (*num_hits == strlen(word))
+	{
+		printf("Congratulations the word is: %s\n", word);
+		exit(0);
 	}
 }
-//intentos fallidos
-void hiting_fails(char letter){
-	fails[num_hits]=letter;
-	num_fails++;
-	attempt--;
-	printf("You fail, only have %d lives\n", attempt);
-	if(attempet==0){
-		printf("LOST\n");
-		printf("The word was %s\n", word);
+
+//tablero de palabra, para mostrarla
+void show_word(char *word, char letter, int *num_hits, char *hidden_word, int *attempts)
+{
+
+	printf("You entered the letter: %c\n", letter);
+	// Sumar uno a los intentos
+	
+	int hit = 0;
+	int i;
+	for (i = 0; i < strlen(word); i++)
+	{
+
+		if (word[i] == letter && hidden_word[i] == '*')
+		{
+			hiting_hits(word, num_hits);
+			hidden_word[i] = letter;
+			hit = 1;
+		}
+
 	}
 
-}
-//anteponer oportunidades
-bool before_hits(char letter){
-	int i;
-	for(i=0; i<num_hits;i++){
-	if(attempt[i]==letter){
-		return true;
-		}
+	if (hit == 0) {
+		*attempts = *attempts + 1;
 	}
-	return false;
+	printf("\n");
 }
-//tablero de palabra, para mostrarla
-void show_word(char letter){
-	int hit = false;
-	int i;
-	for(i=0; i<size;i++){
-	if(words[i]==letter){
-	hiting_hits(letter);
-	printf("%c", letter);
-	hit=true;
-	}else if(before_hits(word[i])){
-		printf(%c,word[i]);
-	}else{
-		printf("*");
-		}
-	}
-}
+
 //funcion principal
-int main(int argc, char** argv){
-	int arg = argc-1;
-	if(arg == 0){
-	instruction();
-	}else
-	int begin = argc;
-	srand(time(NULL));
-	char *word[]= {"Future","Rainbow","Wedding","Strawberry","Nostalgia"}
-	int size=sizeof(word)/(char*);
-	int *word = ramdom(word,size);
-	//falta poner la palabra del azar
-	//creo que falta ose size para otra palabra
-	show_word(letter);
-	system(pause);
-return (0);
+int main(int argc, char **argv)
+{
+
+	int in_game = 1; // 1 en juego, 0 finalizado
+	int lives = 5;
+	int attempt = 0;
+	int num_hits = 0;
+	int size = 0;
+	char letter;
+
+	int arg = argc - 1;
+	if (arg == 0)
+	{
+		instruction();
+	}
+	else
+	{
+		int begin = argc;
+		srand(time(NULL));
+
+		char *word_list[] = {"future", "rainbow", "wedding", "strawberry", "nostalgia"};
+		int size = sizeof(word_list) / sizeof(char *);
+		char *word = get_ramdon(word_list, size);
+
+		// printf("la palabra es: %s\n", word);
+
+		// Convertir la palabra a asteriscos
+
+		char hidden[strlen(word)];
+		int j;
+		for (j = 0; j < strlen(word); j++)
+		{
+			hidden[j] = '*';
+		}
+
+		
+		// Vidas
+		// Que no repita letra -- listo
+		// Que no haya adivinado la palabra
+		
+		while (in_game == 1)
+		{
+
+			if (attempt == lives)
+			{	
+				in_game = 0;
+				printf("You've lost the game. :(\n");
+				printf("The correct word is: %s\n.", word);
+				printf("You only guessed %d letters\n.", num_hits);
+
+			}
+			else
+			{
+				printf("Enter a letter: ");
+				scanf(" %c", &letter);
+				show_word(word, letter, &num_hits, hidden, &attempt);
+
+				printf("\n%s\n\n", hidden);
+
+				printf("lives left: %d/%d\n", (lives-attempt), lives);
+			}
+		}
+	}
+
+	// system(pause);
+	return (0);
 }
+
---
+++ hangman1.c
@@ -0,0 +1,103 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<stdbool.h>
+  //VARIBALES-----------------------
+	char hits[100];
+  char fails[100];
+  int attempt = 6;
+  int num_hits=0;
+  int num_fails=0;
+  int size = 0;
+  //----------------------------
+void instruction(){
+	printf("WELCOME TO HAMGMAN\n");
+	printf("*******\n");
+	printf("You have 5 lives\n");
+	printf("There are some word that could come out: \n");
+        printf("words:\n");
+        printf("    Future\n");
+        printf("    Rainbow\n");
+        printf("    Wedding\n");
+        printf("    Strawberry\n");
+        printf("    Nostalgia\n");
+
+}
+//---------------------------------
+int ramdom(int stop1, int stop2){
+	int valor=rand() % stop1 + stop2;
+	return valor;
+}
+	char *get_ramdon(char *arreglo[], int size){
+	int index = ramdon(size-1,0);
+	char *word=arreglo[index];
+return word;
+}
+//---------------------------------
+void hiting_hits(char letter){
+	hits[num_hits]=letter;
+	num_hits++;
+	if(num_hits==sizeof(word)){
+	printf("Congratulations the word is: %s\n", &word);
+	exit(0);
+	}
+}
+void hiting_fails(char letter){
+	fails[num_hits]=letter;
+	num_fails++;
+	attempt--;
+	printf("You fail, only have %d lives\n", attempt);
+	if(attempet==0){
+		printf("LOST\n");
+		printf("The word was %s\n", word);
+	}
+
+}
+bool before_hits(char letter){
+	int i;
+	for(i=0; i<num_hits;i++){
+	if(attempt[i]==letter){
+		return true;
+		}
+	}
+	return false;
+}
+void show_word(char letter){
+	int hit = false;
+	int i;
+	for(i=0; i<sizeof(word);i++){
+	if(word[i]==letter){
+	hiting_hits(letter);
+	printf("%c", letter);
+	hit=true;
+	}else if(before_hits(word[i])){
+		printf(%c,word[i]);
+	}else{
+		printf("*");
+		}
+	}
+
+}
+int main(int argc, char** argv){
+  //-----------------------------
+	int arg = argc-1;
+	if(arg == 0){
+	instruction();
+	}else
+	int begin = argc;
+  //---------------------------
+	srand(time(NULL));
+	char *words[]= {"Future","Rainbow","Wedding","Strawberry","Nostalgia"};
+	int size=sizeof(words)/(char*);
+	int *word = ramdom(words,size);
+  //-----------------------------
+  system("clear");
+  //int sizetwo=sizeof(word);
+  //---------------------
+    while(attempt>0){
+        printf("Get me a word: \n"); scanf("%c",&letter);
+        show_word(letter);
+  //----------------------
+	system(pause);
+return (0);
+}
